5d6de94e5414a47809811c77ca59a90157af4e9c
Hamidreza Nateghi <hamidrezatemp@gmail.com>
2023-07-26 23:24:00 +0200

    fix lint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import add from '@monorepo/add'
 import MyButton from './components/MyButton'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // made a change
 
   return (
     <>
